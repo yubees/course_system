16504dc9c3e81850c8cac56b15cee1dc5dae156a
yube01 <yubraja008@gmail.com>
2024-04-06 17:44:48 +0545

    initial

---
+++ Models/TodoItem.cs
@@ -0,0 +1,15 @@
+namespace WorkingwithSQLLiteinAsp.NETCoreWebAPI.Models
+{
+    // Models/TodoItem.cs
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+
+        public string Title {get;set;}
+
+
+    }
+
+}
